5a6923b2400ed13a8b8a81da9e3190c937794128
ano <hfxunlp@foxmail.com>
2018-01-08 16:53:05 +0800

    Init

---
+++ OCRServer/OCRServer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using OneNoteOCRDll;
+using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ZeroMQ;
+
+namespace OCRServer
+{
+    public partial class Form1 : Form
+    {
+        private bool running = false;
+        private OneNoteOCR ocr = new OneNoteOCR();
+
+        private string ocrIMG(string imgf, OneNoteOCR eng)
+        {
+            string rs = "";
+            var text = eng.RecognizeImage(imgf);
+            if (text != null)
+            {
+                rs = text;
+            }
+            return rs;
+        }
+        private void server()
+        {
+            if (running)
+            {
+                using (var context = new ZContext())
+                using (var responder = new ZSocket(context, ZSocketType.REP))
+                {
+                    responder.Bind("tcp://" + textBox1.Text);
+                    while (running)
+                    {
+                        using (ZFrame request = responder.ReceiveFrame())
+                        {
+                            responder.Send(new ZFrame(Encoding.UTF8.GetBytes(ocrIMG(request.ReadString(), ocr))));
+                        }
+                    }
+                }
+            }
+        }
+        private void stopServer()
+        {
+            running = false;
+        }
+        private void startServer()
+        {
+            running = true;
+            Thread th = new Thread(server);
+            th.Start();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                stopServer();
+                button1.Text = "启动";
+            }
+            else
+            {
+                startServer();
+                button1.Text = "停止";
+            }
+        }
+    }
+}
---
+++ main.py
@@ -0,0 +1,189 @@
+#-*-coding:utf-8-*-
+import zmq
+import subprocess,os,sys
+import cv2
+import urllib2,gzip
+from StringIO import StringIO
+from urllib import quote
+
+globalStartupInfo = subprocess.STARTUPINFO()
+globalStartupInfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+def runCmd(cmd):
+	p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.getcwd(), shell=False, startupinfo=globalStartupInfo)
+	p.wait()
+	rs=p.stdout.read().decode()
+	return rs
+
+def capture(rsf):
+	sdcardpath='/sdcard/screenshot.jpg'
+	if os.path.exists(rsf):
+		os.remove(rsf)
+	jtcmd='adb.exe shell /system/bin/screencap -p '+sdcardpath
+	runCmd(jtcmd)
+	jtcmd='adb.exe pull '+sdcardpath+' '+rsf
+	runCmd(jtcmd)
+	jtcmd='adb.exe shell rm '+sdcardpath
+	runCmd(jtcmd)
+
+def segPic(srcf, rsf, startx, endx, starty, endy):
+	image = cv2.imread(srcf)
+	rs = image[startx:endx, starty:endy]
+	cv2.imwrite(rsf, rs)
+
+def getCon(addr):
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.connect("tcp://"+addr)
+	return socket
+
+def touch(x, y):
+	runCmd("adb.exe shell input tap "+str(x)+" "+str(y))
+
+def ocr(addr, imgf):
+	con = getCon(addr)
+	con.send(imgf)
+	rs = con.recv()
+	return rs.decode("utf-8", "ignore")
+
+def getPage(wd):
+	header = {
+	"Host":"www.sogou.com",
+	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
+	"DNT":"1",
+	"Connection":"keep-alive",
+	"Accept-Language":"zh-CN,zh;q=0.9",
+	"Accept-Encoding":"gzip, deflate, br",
+	"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+	}
+	req = urllib2.Request("https://www.sogou.com/web?query="+quote(wd.encode("utf-8")),headers=header)
+	r = urllib2.urlopen(req)
+	rs = u""
+	if r.info().get('Content-Encoding')=='gzip':
+		buf = StringIO(r.read())
+		f = gzip.GzipFile(fileobj = buf)
+		rs = f.read()
+		f.close()
+	else:
+		rs = r.read()
+	r.close()
+	return rs.decode("utf-8")
+
+def getCount(wd):
+	try:
+		page = getPage(wd)
+		page = page[page.find(u"搜狗已为您找到")+8:]
+		page = page[:page.find(u"条相关结果")]
+		rs = int(page.replace(",", ""))
+	except:
+		rs = 0
+	return rs
+
+def cleanQ(strin, trimHead = True):
+	if trimHead and len(strin)>1 and (strin[0].lower() in ["a", "b", "c", "d"]):
+		tmp=strin[1:]
+	else:
+		tmp=strin
+	return tmp.replace(" ", "")
+
+def parseOCR(txtin):
+	q = ""
+	t = []
+	tmp = txtin.split("\n")
+	tmp1 = []
+	for tmpu in tmp:
+		tt = tmpu.strip()
+		if tt:
+			tmp1.append(tt)
+	nline = len(tmp1)
+	if nline < 4:
+		tmp = tmp1[0].split()
+		nd = len(tmp)
+		ntotal = nd + nline - 1
+		if ntotal < 4:
+			t = tmp
+			t.extend([tmp[-1] for i in xrange(3 - nline - nd)])
+			t.extend(tmp1[1:])
+			q = tmp[0]
+		else:
+			t = tmp[nd - 4 + nline:]
+			t.extend(tmp1)
+	else:
+		t=tmp1[nline-4:]
+		q="".join(tmp1[:nline-4])
+	return q.replace(" ", ""), [cleanQ(tu, True) for tu in t]	
+
+def choose(ans):
+	lind={1:100, 2:200, 3:300, 4:400}
+	rind={1:100, 2:200, 3:300, 4:400}
+	touch(lind[ans], rind[ans])
+
+def getAnswer(q, t):
+	maxscore = 0
+	ans = 1
+	curid = 1
+	for tu in t:
+		qr = q+" "+tu
+		print("Query "+str(curid)+" is "+qr)
+		curs = float(getCount(qr))/float(getCount(tu))
+		#curs = getCount(qr)
+		print(">>Score of candidate "+str(curid)+": "+ str(curs))
+		if curs > maxscore:
+			maxscore = curs
+			ans = curid
+		curid += 1
+	return ans
+
+def main():
+	run=True
+	while run:
+		cmd=raw_input(">>")
+		if cmd.lower()=="exit":
+			run=False
+			break
+		else:
+			print(">>Getting screenshot")
+			capture("screen.jpg")
+			print(">>Focus on QA")
+			segPic("screen.jpg", "center.jpg", 200, 1720, 0, 1080)
+			print(">>run OCR")
+			content = ocr("localhost:9355", "center.jpg")
+			print(">>OCR result:")
+			print(content)
+			print(">>parsing OCR results")
+			q, t = parseOCR(content)
+			print(">>Question:")
+			print(q)
+			print(">>Query:")
+			print(t)
+			ans = getAnswer(q, t)
+			print("Select "+str(ans))
+			choose(ans)
+			print("Done")
+
+def testOCR():
+	rs = ocr("localhost:9355", "test.jpg")
+	print(type(rs))
+	print(len(rs))
+	print(rs.encode("gbk", "ignore"))
+	with open("rec.txt", "w") as f:
+		f.write(rs.encode("utf-8"))
+
+def testCapture():
+	capture("screen.jpg")
+
+def testSeg():
+	segPic("screen.jpg", "center.jpg", 120, 1800, 0, 1080)
+
+def tesTouch():
+	touch(360, 409)
+
+def testPage():
+	with open("test.html", "w") as f:
+		f.write(getPage(u"百度云"))
+
+def testCount():
+	print(getCount(u"百度云"))
+
+if __name__ == "__main__":
+	main()
